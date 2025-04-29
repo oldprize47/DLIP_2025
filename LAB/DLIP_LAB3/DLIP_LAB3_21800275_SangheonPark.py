f78afb0fb104d76db5d84dfbfac429639b662cfa
Sangheon Park <qkrwkdrns01@gmail.com>
2025-04-29 13:21:43 +0900

    fixed Lab3

--- LAB/DLIP_LAB3/DLIP_LAB3_21800275_SangheonPark.py
+++ LAB/DLIP_LAB3/DLIP_LAB3_21800275_SangheonPark.py
@@ -1,14 +1,66 @@
+'''
+@ Deep Learning & Image Processing  by Young-Keun Kim - Handong Global University
+
+Author          : Sangheon Park
+Created         : 2025-04-22
+Modified        : 2025-04-30
+Language/ver	: Python
+Course		: Deep Learning & Image Processing
+
+Description     : DLIP_LAB3_21800275_SangheonPark.cpp
+
+'''
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
-def onChange(pos):
-    pass
+def valid_cont_point(contour):
+    arr_point = np.empty([0,2])
 
-def func(x,a,b,c,d,e):		#curve_fitting에 사용할 함수 사전 정의
+    for k in contour:
+        retval = cv2.arcLength(k,0)
+        if retval > 950:
+            cv2.drawContours(cont_img, k, -1, (0,255,0), 4)
+            for j in k:
+                cv2.circle(cont_img, tuple(j[0]), 1, (255,0,0), -1)
+                arr_point = np.vstack([arr_point,j[0]])
+    return arr_point
+
+def get_curve_fit_point(arr_point):
+    global maxpoint
+    x = arr_point[:,0]
+    y = arr_point[:,1]
+
+    popt, pcov = curve_fit(func,x,y)
+    fit_x = np.linspace(min(x),max(x),5000)
+    cur_y = func(fit_x,popt[0],popt[1],popt[2], popt[3], popt[4])
+
+    maxpoint = max(cur_y)
+
+    fit_x = fit_x.reshape(-1,1)
+    cur_y = cur_y.reshape(-1,1)
+    
+    return fit_x, cur_y
+
+def det_level(score):
+    if score < 120:
+        level = 3  
+    elif score < 250:
+        level = 2
+    else:
+        level = 1
+    return level
+
+def write_text(img, score, level):
+    cv2.rectangle(img, (int(img.shape[1]/2 - 10 ), int(img.shape[0] / 4) - 30), (int(img.shape[1]/2) + 250, int(img.shape[0] / 4) + 50), (0,255,0), 2, cv2.LINE_8)
+    cv2.putText(img, f"Score : {score}", (int(img.shape[1]/2), int(img.shape[0] / 4)), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
+    cv2.putText(img, f"Level : {level}", (int(img.shape[1]/2 + 11), int(img.shape[0] / 4 + 40)), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
+
+def func(x,a,b,c,d,e):		#curve_fitting func
     return a*x**4 + b*x**3 + c*x**2 + d*x+e
 
+# Main Start
 src = []
 for i in range(3):
     img = cv2.imread(f"LV{i+1}.png")
@@ -17,81 +69,55 @@ for i in range(3):
     src_arr = np.stack(src, axis=0)
 
     img = src_arr[i].copy()
-    img1 = img.copy()
+    cont_img = img.copy()
 
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    plt.imshow(img_rgb)
-    plt.title(f'Source - LV{i+1}')
-    plt.axis('off')
-    plt.show()
-    # cv2.namedWindow(f'Source - {i}', 0)
-    # cv2.imshow(f'Source - {i}', img)
+    # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.namedWindow(f'Source - {i}', 0)
+    cv2.imshow(f'Source - {i}', img)
 
-    T2 = 200
-    T1 = 50
+    T1 = 200
+    T2 = 50
 
     b, g, r = cv2.split(img)
     temp = r.copy()
 
     # /// Reduce noise with a kernel 3x3
-    temp = cv2.GaussianBlur(temp, (3, 3), 5)
+    blured = cv2.GaussianBlur(temp, (3, 3), 5)
 
-    canny_img = cv2.Canny(temp, T1, T2)
-    cv2.rectangle(canny_img, (int(temp.shape[1] / 2), 0), (int(temp.shape[1]), int(temp.shape[0])), 0, cv2.FILLED)
-    cv2.rectangle(canny_img, (0, 0), (int(temp.shape[1] / 4), int(temp.shape[0] * 5 / 12)), 0, cv2.FILLED)
-    cv2.rectangle(canny_img, (0, 0), (int(temp.shape[1]), int(temp.shape[0] * 3 / 12)), 0, cv2.FILLED)
-    pts1 = np.array([[int(temp.shape[1] / 2), 0], [int(temp.shape[1] * 5 / 12), int(temp.shape[0])], [int(temp.shape[1]), int(temp.shape[0])]])
+    canny_img = cv2.Canny(blured, T2, T1)
+
+    cv2.rectangle(canny_img, (int(canny_img.shape[1] / 2), 0), (int(canny_img.shape[1]), int(canny_img.shape[0])), 0, cv2.FILLED)
+    cv2.rectangle(canny_img, (0, 0), (int(canny_img.shape[1] / 4), int(canny_img.shape[0] * 5 / 12)), 0, cv2.FILLED)
+    cv2.rectangle(canny_img, (0, 0), (int(canny_img.shape[1]), int(canny_img.shape[0] * 3 / 12)), 0, cv2.FILLED)
+    pts1 = np.array([[int(canny_img.shape[1] / 2), 0], [int(canny_img.shape[1] * 5 / 12), int(canny_img.shape[0])], [int(canny_img.shape[1]), int(canny_img.shape[0])]])
     cv2.fillPoly(canny_img, [pts1], 0)
-    pts2 = np.array([[int(temp.shape[1] * 7 / 12), 0], [int(temp.shape[1] * 4 / 12), int(temp.shape[0])], [int(temp.shape[1]), int(temp.shape[0])]])
+    pts2 = np.array([[int(canny_img.shape[1] * 7 / 12), 0], [int(canny_img.shape[1] * 4 / 12), int(canny_img.shape[0])], [int(canny_img.shape[1]), int(canny_img.shape[0])]])
     cv2.fillPoly(canny_img, [pts2], 0)
 
     element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3)) # cv.MORPH_ELLIPSE, cv.MORPH_CROSS, cv.MORPH_RECT
-    canny_img = cv2.morphologyEx(canny_img, cv2.MORPH_DILATE, element, canny_img, (-1,-1),1)     # cv.MORPH_ERODE, cv.MORPH_DILATE, cv.MORPH_OPEN, cv.MORPH_CLOSE, cv.MORPH_GRADIENT
-    
-    contour, hierarchy = cv2.findContours(canny_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    morp_img = cv2.morphologyEx(canny_img, cv2.MORPH_DILATE, element)     # cv.MORPH_ERODE, cv.MORPH_DILATE, cv.MORPH_OPEN, cv.MORPH_CLOSE, cv.MORPH_GRADIENT
 
-    arr_point = np.empty([0,2])
+    contour, hierarchy = cv2.findContours(morp_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    for k in contour:
-        retval = cv2.arcLength(k,0)
-        if retval > 950:
-            cv2.drawContours(img1, k, -1, (0,255,0), 4)
-            for j in k:
-                cv2.circle(img1, tuple(j[0]), 1, (255,0,0), -1)
-                arr_point = np.vstack([arr_point,j[0]])
+    arr_point = valid_cont_point(contour)
 
-    x = arr_point[:,0]
-    y = arr_point[:,1]
+    fit_x, cur_y = get_curve_fit_point(arr_point)
 
-    popt, pcov = curve_fit(func,x,y)
-    fit_x = np.linspace(min(x),max(x),5000)
-    cur_y = func(fit_x,popt[0],popt[1],popt[2], popt[3], popt[4])
-    score = img.shape[0] - max(cur_y)
-
-    fit_x = fit_x.reshape(-1,1)
-    cur_y = cur_y.reshape(-1,1)
-
+    #Draw Curve_fit Func on Image
     arr_cur = np.concatenate((fit_x, cur_y), axis=1)
+    cv2.polylines(img, [arr_cur.astype(np.int32)], False, (0, 255, 0), 4)
 
-    cv2.polylines(img_rgb, [arr_cur.astype(np.int32)], False, (0, 255, 0), 4)
+    score = round(float(img.shape[0] - maxpoint), 2)
+    level = det_level(score)
 
-    if score < 120:
-        level = 3
-    elif score < 250:
-        level = 2
-    else:
-        level = 1
+    write_text(img, score, level)
 
-    cv2.rectangle(img_rgb, (int(img_rgb.shape[1]/2 - 10 ), int(img_rgb.shape[0] / 4) - 30), (int(img_rgb.shape[1]/2) + 250, int(img_rgb.shape[0] / 4) + 50), (0,255,0), 2, cv2.LINE_8)
-    cv2.putText(img_rgb, f"Score : {round(score,2)}", (int(img_rgb.shape[1]/2), int(img_rgb.shape[0] / 4)), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
-    cv2.putText(img_rgb, f"Level : {level}", (int(img_rgb.shape[1]/2 + 11), int(img_rgb.shape[0] / 4 + 40)), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
+    cv2.namedWindow(f'Final - {i}', 0)
+    cv2.imshow(f'Final - {i}', img)
 
-    # cv2.namedWindow(f"CHAIN_APPROX_SIMPLE - {i}", 0)
-    # cv2.imshow(f"CHAIN_APPROX_SIMPLE - {i}", img1)
+while True:
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:  # ESC key
+        break
 
-    plt.imshow(img_rgb)
-    plt.title(f'Result - LV{i+1}')
-    plt.axis('off')
-    plt.show()
-    # cv2.namedWindow(f'Final - {i}', 0)
-    # cv2.imshow(f'Final - {i}', img)
+cv2.destroyAllWindows()
